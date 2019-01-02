6b7b28e849d3b5cf801d8b9c5f8bd46e02bc5d6f
Bryan Lai <bryanlais@gmail.com>
2019-01-03 00:15:53 +0800

    Add util to spin down HDD

---
+++ .config/argos/aeject.0r.1d+.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Spin down HDD - Argos Widget
+
+echo "| iconName=media-removable"
+echo "---"
+
+fontset="font='Anka/Coder Condensed' size=11.5"
+bashcmd="bash='gnome-terminal -- udisksctl-off' terminal=false"
+
+if [ "$ARGOS_MENU_OPEN" == "true" ]; then
+
+	export GREP_COLORS=ne
+	drives_info=$(lsblk-more \
+		| GREP_COLOR='01;33' grep -E --color=always '/run/media/|$' \
+		| awk 1 ORS="\\\\n" \
+		| sed -e 's/ *\\n/\\n/g' \
+		| head -c -2)  # Remove trailing whitespace & the last newline
+
+	echo "<span weight='bold'>$drives_info</span> | $fontset $bashcmd"
+
+else
+	echo "Loading... | $fontset $bashcmd"
+fi
+
