d46408138abe0aae40cd983603c1a4e1fe3cdba5
Bryan Lai <bryanlais@gmail.com>
2019-01-11 00:38:05 +0800

    Complete revamp .shrc for usability; hide AQI widget when no internet

--- .config/argos/z_aqi.1000c.30m+.sh
+++ .config/argos/z_aqi.1000c.30m+.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Wrapper for Argos AQI Widget
 
+# Don't bother when no network
+if nmcli --get-values=STATE general status \
+	| grep --quiet -E 'local only|disconnected'; then
+		echo ""
+		echo "---"
+		exit 0
+fi
+
 # Update python path & tokens
 source "$HOME/.shrc"
 export TOKEN_AQICN
@@ -8,4 +16,3 @@ export TOKEN_AQICN
 # Important: correct path
 cd "$HOME/.config/argos/aqi"
 ./widget.py
-
