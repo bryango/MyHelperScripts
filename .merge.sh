1e8559d9b5271fa466753c6d516324c5ee3fa26c
Bryan Lai <bryanlais@gmail.com>
2019-01-13 11:13:52 +0800

    Update merge script

--- .merge.sh
+++ .merge.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# Merge HOME to publish
+# Selectively merge HOME to publish
+
+function gstats { git status -M --porcelain; }
 
 if [[ "$(git rev-parse --abbrev-ref HEAD)" != "publish" ]]; then
 	echo "# Not on publish branch! Aborted."
 	exit 1
-elif stats=$(git status -M --porcelain) && [[ -z "$stats" ]]; then
-	git merge --squash --no-commit HOME
+elif git merge HEAD &>/dev/null; then
 	git reset HEAD .
-	echo "$stats" \
+	gstats \
 		| sed --quiet 's/^ *M *//p' \
 		| while IFS= read -r line || [[ -n "$line" ]]; do
 			git add "$line";
 		done
 else
-	echo "# Workspace is not clean! Clean up first."
-	exit 1
+	echo "# Still merging! Resolve conflicts first."
+	exit 1  # https://stackoverflow.com/a/30781568/10829731
 fi
